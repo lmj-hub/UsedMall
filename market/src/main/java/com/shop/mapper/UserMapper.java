cf2847494475473aaf763af9415aade59e87fd29
yyuhao <0adc09279c00f4c11f1cc004aee38744@huawei.com>
2021-06-03 15:26:10 +0800

    多余

--- market/src/main/java/com/shop/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.shop.mapper;
-
-import com.shop.base.BaseDao;
-import com.shop.po.User;
-
-public interface UserMapper extends BaseDao<User> {
-
-}
--- market/src/main/java/com/shop/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.shop.service;
-
-import com.shop.base.BaseService;
-import com.shop.po.User;
-
-public interface UserService extends BaseService<User> {
-}
