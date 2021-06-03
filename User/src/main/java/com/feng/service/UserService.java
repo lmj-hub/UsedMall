6c9101b0e36c2293c5978b96aa65edf5af774375
yyuhao <0adc09279c00f4c11f1cc004aee38744@huawei.com>
2021-06-03 15:55:40 +0800

    。

--- User/src/main/java/com/feng/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.feng.service;
-
-import com.feng.model.User;
-
-public interface UserService {
-    User selectUserByUserName(String username);
-    boolean insertUser(User user);
-    boolean delUser(User user);
-}
